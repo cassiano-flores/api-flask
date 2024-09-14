1dc8d50f5c9082fe88b42ff3cbab802fdd8f94ad
cassiano-flores <cassiano.flores2424@gmail.com>
2024-09-14 12:33:05 -0300

    steps 1 and 2 done

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 import pandas as pd
-from flask import Flask, jsonify
+from flask import Flask, jsonify, request
 
 CSV_FILE = "nba_players.csv"
 DATASET = pd.read_csv(CSV_FILE, sep=";")
@@ -24,9 +24,42 @@ def get_player(id):
         return jsonify({'message': 'Player not found'}), 404
 
 
-# PUT
-# POST
-# DELETE
+# PUT - atualiza um jogador especifico pelo indice (ID)
+@app.route('/player/<int:id>', methods=['PUT'])
+def update_player(id):
+    if 0 <= id < len(DATASET):
+        new_data = request.json
+        for key in new_data.keys():
+            DATASET.at[id, key] = new_data[key]
+
+        player = DATASET.iloc[id].to_dict()
+        return jsonify(player), 200
+    else:
+        return jsonify({'message': 'Player not found'}), 404
+
+
+# POST - adiciona novo jogador
+@app.route('/player', methods=['POST'])
+def add_player():
+    global DATASET
+
+    new_player = pd.DataFrame([request.json])
+    DATASET = pd.concat([DATASET, new_player], ignore_index=True)
+
+    player = DATASET.iloc[-1].to_dict()
+    return jsonify(player), 200
+
+
+# DELETE - remove um jogador especifico pelo indice (ID)
+@app.route('/player/<int:id>', methods=['DELETE'])
+def remove_player(id):
+    global DATASET
+
+    if 0 <= id < len(DATASET):
+        DATASET = DATASET.drop(index=id).reset_index(drop=True)
+        return jsonify({'message': f'Player with index {id} has been deleted.'}), 200
+    else:
+        return jsonify({'message': 'Player not found'}), 404
 
 
 if __name__ == '__main__':
